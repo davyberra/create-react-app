c41189fb78139d97566439b1a995e358f4449c6c
unknown <davyberra@gmail.com>
2021-04-20 21:16:31 -0700

    finished lesson 7

--- src/index.js
+++ src/index.js
@@ -6,7 +6,34 @@ import reportWebVitals from './reportWebVitals';
 
 const root = document.querySelector("#root");
 
-ReactDOM.render(React.createElement("p", {}, "Hello, World!"), root);
+const user = {
+    firstName: "Davy",
+    lastName: "Berra",
+};
+
+function Header() {
+    return <h1>Hello, {user.firstName} {user.lastName}!</h1>;
+};
+
+function Body() {
+    return (
+        <>
+            <p>This is the body.</p>
+            <br />
+            <p>This is more of the body!</p>
+        </>
+    )
+
+};
+
+const element =
+    <>
+        <Header />
+        <Body />
+    </>;
+
+
+ReactDOM.render(element, root);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
